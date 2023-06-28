fd5af1a0eca9fe65c71799016b919a8363a51341
ekeneakubue <ekeneakubue@gmail.com>
2023-06-28 22:40:05 +0100

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className='container-fluid'>
       <div className="row my-footer">
         <div className="foot-left">
-          <a href="https://wa.me/+2348032744865" target="_blank" rel="noopener npreferrer"><div className="foot-icon text-success"><i class="bi bi-whatsapp"></i></div></a>         
+          <a href="https://wa.me/+2348067359106" target="_blank" rel="noopener npreferrer"><div className="foot-icon text-success"><i class="bi bi-whatsapp"></i></div></a>         
           <div className="foot-icon text-primary"><i class="bi bi-facebook"></i></div>          
           <div className="foot-icon text-info"><i class="bi bi-telegram"></i></div>
           <div className="foot-icon text-primary"><i class="bi bi-twitter"></i></div>
